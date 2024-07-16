f80cb782d7324160faa166d1f6bd5b52650f3c59
Phathihi <128897124+peolodai123@users.noreply.github.com>
2024-07-16 08:40:15 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'hello.90zonzx.mongodb.net',
+  DB_USER: 'phatppp',
+  DB_PASS: '01112003',
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: 'phatppp2003',
+  JWT_EXPIRES: '3600000', // in milliseconds
+  EMAIL_USER: 'truongthinhphat0111@gmail.com', // gmail service
+  EMAIL_PASS: 'rsxj ilcz itcd whpa'
+};
+module.exports = MyConstants;
